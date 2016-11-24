fd5a98a12d786a81845dfaca4e43293a3c0c4218
Stephen Doxsee <stephen@doxsee.org>
2016-11-23 22:57:34 -0500

    Auto-add creation date. Make it uneditable

--- src/main/java/com/mycompany/myapp/web/rest/ItemResource.java
+++ src/main/java/com/mycompany/myapp/web/rest/ItemResource.java
@@ -1,11 +1,13 @@
 package com.mycompany.myapp.web.rest;
 
-import com.codahale.metrics.annotation.Timed;
-import com.mycompany.myapp.domain.Item;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.time.ZonedDateTime;
+import java.util.List;
+import java.util.Optional;
 
-import com.mycompany.myapp.repository.ItemRepository;
-import com.mycompany.myapp.web.rest.util.HeaderUtil;
-import com.mycompany.myapp.web.rest.util.PaginationUtil;
+import javax.inject.Inject;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
@@ -14,13 +16,17 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.inject.Inject;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.List;
-import java.util.Optional;
+import com.codahale.metrics.annotation.Timed;
+import com.mycompany.myapp.domain.Item;
+import com.mycompany.myapp.repository.ItemRepository;
+import com.mycompany.myapp.web.rest.util.HeaderUtil;
+import com.mycompany.myapp.web.rest.util.PaginationUtil;
 
 /**
  * REST controller for managing Item.
@@ -30,7 +36,7 @@ import java.util.Optional;
 public class ItemResource {
 
     private final Logger log = LoggerFactory.getLogger(ItemResource.class);
-        
+
     @Inject
     private ItemRepository itemRepository;
 
@@ -49,6 +55,9 @@ public class ItemResource {
         log.debug("REST request to save Item : {}", item);
         if (item.getId() != null) {
             return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert("item", "idexists", "A new item cannot already have an ID")).body(null);
+        }
+        if (item.getCreationDate() == null) {
+            item.creationDate(ZonedDateTime.now());
         }
         Item result = itemRepository.save(item);
         return ResponseEntity.created(new URI("/api/items/" + result.getId()))
